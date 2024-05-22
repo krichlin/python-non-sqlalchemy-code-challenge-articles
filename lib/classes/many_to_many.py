41bb70195530eede2c4fb1974593acc99327f96c
Ken Richlin <krichlin@gmail.com>
2024-05-22 00:00:08 -0400

    started challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,10 +3,32 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, new_title):
+        if not isinstance(new_title, str):
+            raise TypeError("Title must be of type str")
+        if len(new_title) < 5 or len(new_title) > 50:
+            raise ValueError("Title must be between 5 and 50 characters inclusive")
+        self._title = new_title
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise TypeError("Name must be of type str")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+
 
     def articles(self):
         pass
@@ -25,6 +47,31 @@ class Magazine:
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError("Name must be of type str")
+        if len(new_name) < 2 or len(new_name) > 16:
+            raise ValueError("Name must be between 2 and 16 characters, inclusive")
+        self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, new_category):
+        if not isinstance(new_category, str):
+            raise TypeError("Category must be of type str")
+        if len(new_category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._category = new_category
+
+    
     def articles(self):
         pass
 
