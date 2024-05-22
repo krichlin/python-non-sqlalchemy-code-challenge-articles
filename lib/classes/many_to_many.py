9644e9bee5b62f8cb121d300b36274dfc7e0797e
Ken Richlin <krichlin@gmail.com>
2024-05-22 09:35:22 -0400

    16 tests passing

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,13 +1,19 @@
 class Article:
 
     all = []
+    all_articles = []
 
     def __init__(self, author, magazine, title):
         self.author = author
+
         self.magazine = magazine
         self.title = title
         Article.add_new_article(self)
     
+    #@property 
+    #def author(self):
+    #    return self._author
+
     @property
     def title(self):
         return self._title
@@ -23,6 +29,7 @@ class Article:
     @classmethod
     def add_new_article(cls, new_article):
         cls.all.append(new_article)
+        cls.all_articles.append(new_article)
 
 
 class Author:
@@ -37,24 +44,33 @@ class Author:
     def name(self):
         return self._name
     
-
-
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self,magazine,title)
 
     def topic_areas(self):
-        pass
+        articles = self.articles()
+        if not articles:
+            return None
+        return list(set(article.magazine.category for article in articles))
+
+
+
 
 class Magazine:
+
+    all_magazines = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
+        Magazine.all_magazines.append(self) # add the magazine instance to all_magazines list on instantiation
 
     @property
     def name(self):
@@ -79,16 +95,30 @@ class Magazine:
         if len(new_category) == 0:
             raise ValueError("Category must be longer than 0 characters")
         self._category = new_category
-
     
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
-
+        authors = [article.author for article in self._articles]
+        return list(set(authors))
+    
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles] 
 
     def contributing_authors(self):
-        pass
+        authors = [article.author for article in self._articles]
+        unique_authors = set(authors)
+        return [author for author in unique_authors if authors.count(author) > 2]
+    
+    def add_article(self, author, title):
+        article = Article(author, self, title) # create article instantce
+        self._articles.append(article) #  add new article to all list
+        return article
+    
+    @classmethod
+    def top_publisher(cls):
+        if not cls.all.magazines:
+            return None
+        return max(cls.all_magazines, key=lambda mag: len(mag.articles()))
+    
